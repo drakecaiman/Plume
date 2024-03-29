9e7f561ddf4e3925ca7cdec5b6d66e671a8e2064
Duncan Oliver <duncan.oliver@gmail.com>
2024-03-28 17:51:31 -0700

    Support for compatible `Range`s

--- Sources/Plume/Property Wrappers/Clamped.swift
+++ Sources/Plume/Property Wrappers/Clamped.swift
@@ -14,35 +14,25 @@ import Foundation
 {
     var wrappedValue: Value
     {
-        didSet
-        {
-            switch range
-            {
-                case .open(range: let openRange):
-//              TODO: Replace with use of `Range.clamp()`
-                wrappedValue = openRange.lowerBound
-//                wrappedValue = openRange.clamp(wrappedValue)
-                case .closed(range: let closedRange):
-                wrappedValue = closedRange.clamp(wrappedValue)
-            }
-        }
+        didSet { self.clampWrappedValue() }
     }
     
-    enum RangeType {
-        typealias SignedIntegerStrideableBound = Value where Value : Strideable, Value.Stride : SignedInteger
-//        case open(range: Range<SignedIntegerStrideableBound>)
-        case open(range: Range<Value>)
-        case closed(range: ClosedRange<Value>)
-    }
-    let range : RangeType
+    private let range : ClosedRange<Value>
     
-    init(wrappedValue: Value, range: Range<Value>) {
-        self.wrappedValue = wrappedValue
-        self.range = .open(range: range)
+    init(wrappedValue: Value, range: Range<Value>) where Value : Strideable, Value.Stride : SignedInteger
+    {
+        self.init(wrappedValue: wrappedValue, range: ClosedRange(range))
     }
     
     init(wrappedValue: Value, range: ClosedRange<Value>) {
+        self.range = range
+//      TODO: Can this be reduced
         self.wrappedValue = wrappedValue
-        self.range = .closed(range: range)
+        self.clampWrappedValue()
+    }
+    
+    private mutating func clampWrappedValue()
+    {
+        self.wrappedValue = range.clamp(wrappedValue)
     }
 }
