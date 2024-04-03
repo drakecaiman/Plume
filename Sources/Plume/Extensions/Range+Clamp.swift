96866c41d6f3190139516043c6e9d1f9f32d4870
Duncan Oliver <duncan.oliver@gmail.com>
2024-04-03 16:32:37 -0700

    Added documentation to "Range+Clamp"

--- Sources/Plume/Extensions/Range+Clamp.swift
+++ Sources/Plume/Extensions/Range+Clamp.swift
@@ -8,6 +8,13 @@
 import Foundation
 
 extension Range where Bound : Strideable, Bound.Stride : SignedInteger {
+    /**
+     Clamp a value to this range.
+
+     - Parameters:
+        - value: The value to clamp.
+     - Returns: The value clamped to the bounds of this range.
+     */
     func clamp(_ value: Bound) -> Bound {
         return ClosedRange(self).clamp(value)
     }
