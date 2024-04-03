49f542c7b32ee14fa449006132eeb468caa41c30
Duncan Oliver <duncan.oliver@gmail.com>
2024-04-03 16:29:01 -0700

    Add documentation to "ClosedRange+Clamp"

--- Sources/Plume/Extensions/ClosedRange+Clamp.swift
+++ Sources/Plume/Extensions/ClosedRange+Clamp.swift
@@ -10,7 +10,11 @@ import Foundation
 extension ClosedRange
 {
     /**
+     Clamp a value to this range.
      
+     - Parameters:
+        - value: The value to clamp.
+     - Returns: The value clamped to the bounds of this range.
      */
     func clamp(_ value: Bound) -> Bound
     {
