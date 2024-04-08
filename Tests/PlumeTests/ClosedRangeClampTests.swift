0d5f4935e2a4fb497497ae735d05b808cc4caaf9
Duncan Oliver <duncan.oliver@gmail.com>
2024-04-07 17:42:45 -0700

    Implement tests for `ClosedRange.clamp(:)`

--- Tests/PlumeTests/ClosedRangeClampTests.swift
+++ Tests/PlumeTests/ClosedRangeClampTests.swift
@@ -8,32 +8,132 @@
 import XCTest
 
 final class ClosedRangeClampTests: XCTestCase {
+    static let intRange = 0...100
+    static let floatRange : ClosedRange<Float> = 0.0...1000.0
+    static let characterRange : ClosedRange<Character> = "c"..."q"
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testIntClosedRangeClampInRange()
+    {
+        let initialValue = 52
+        let result = ClosedRangeClampTests.intRange.clamp(initialValue)
+        XCTAssertEqual(initialValue, result, "Clamping result \(result) should equal initial in-range value \(initialValue)")
     }
     
-    func testNoMax() throws {
-//        (0...).clamp
+    func testIntClosedRangeClampAboveRange()
+    {
+        let initialValue = 1274
+        let result = ClosedRangeClampTests.intRange.clamp(initialValue)
+        XCTAssertNotEqual(initialValue, result, "Clamping result \(result) should not equal initial in-range value \(initialValue).")
+        guard let expected = ClosedRangeClampTests.intRange.last else
+        {
+            XCTFail("Could not get expected value.")
+            return
+        }
+        XCTAssertEqual(result, expected, "Clamping result \(result) should equal expected value \(expected).")
     }
-
+    
+    func testIntClosedRangeClampBelowRange()
+    {
+        let initialValue = -37
+        let result = ClosedRangeClampTests.intRange.clamp(initialValue)
+        XCTAssertNotEqual(initialValue, result, "Clamping result \(result) should not equal initial in-range value \(initialValue).")
+        guard let expected = ClosedRangeClampTests.intRange.first else
+        {
+            XCTFail("Could not get expected value.")
+            return
+        }
+        XCTAssertEqual(result, expected, "Clamping result \(result) should equal expected value \(expected).")
+    }
+    
+    func testIntClosedRangeClampOnUpperBound()
+    {
+        let initialValue = ClosedRangeClampTests.intRange.upperBound
+        let result = ClosedRangeClampTests.intRange.clamp(initialValue)
+        XCTAssertEqual(initialValue, result, "Clamping result \(result) should equal initial in-range value \(initialValue)")
+    }
+    
+    func testIntClosedRangeClampOnLowerBound()
+    {
+        let initialValue = ClosedRangeClampTests.intRange.lowerBound
+        let result = ClosedRangeClampTests.intRange.clamp(initialValue)
+        XCTAssertEqual(initialValue, result, "Clamping result \(result) should equal initial in-range value \(initialValue)")
+    }
+    
+    func testFloatClosedRangeClampInRange()
+    {
+        let initialValue : Float = 639.25
+        let result = ClosedRangeClampTests.floatRange.clamp(initialValue)
+        XCTAssertEqual(initialValue, result, "Clamping result \(result) should equal initial in-range value \(initialValue)")
+    }
+    
+    func testFloatClosedRangeClampAboveRange()
+    {
+        let initialValue : Float = 19230.128
+        let result = ClosedRangeClampTests.floatRange.clamp(initialValue)
+        XCTAssertNotEqual(initialValue, result, "Clamping result \(result) should not equal initial in-range value \(initialValue).")
+        let expected = ClosedRangeClampTests.floatRange.upperBound
+        XCTAssertEqual(result, expected, "Clamping result \(result) should equal expected value \(expected).")
+    }
+    
+    func testFloatClosedRangeClampBelowRange()
+    {
+        let initialValue : Float = -1612.27
+        let result = ClosedRangeClampTests.floatRange.clamp(initialValue)
+        XCTAssertNotEqual(initialValue, result, "Clamping result \(result) should not equal initial in-range value \(initialValue).")
+        let expected = ClosedRangeClampTests.floatRange.lowerBound
+        XCTAssertEqual(result, expected, "Clamping result \(result) should equal expected value \(expected).")
+    }
+    
+    func testFloatClosedRangeClampOnUpperBound()
+    {
+        let initialValue = ClosedRangeClampTests.floatRange.upperBound
+        let result = ClosedRangeClampTests.floatRange.clamp(initialValue)
+        XCTAssertEqual(initialValue, result, "Clamping result \(result) should equal initial in-range value \(initialValue)")
+    }
+    
+    func testFloatClosedRangeClampOnLowerBound()
+    {
+        let initialValue = ClosedRangeClampTests.floatRange.lowerBound
+        let result = ClosedRangeClampTests.floatRange.clamp(initialValue)
+        XCTAssertEqual(initialValue, result, "Clamping result \(result) should equal initial in-range value \(initialValue)")
+    }
+    
+    func testCharacterClosedRangeClampInRange()
+    {
+        let initialValue : Character = "d"
+        let result = ClosedRangeClampTests.characterRange.clamp(initialValue)
+        XCTAssertEqual(initialValue, result, "Clamping result \(result) should equal initial in-range value \(initialValue)")
+    }
+    
+    func testCharacterClosedRangeClampAboveRange()
+    {
+        let initialValue : Character = "x"
+        let result = ClosedRangeClampTests.characterRange.clamp(initialValue)
+        XCTAssertNotEqual(initialValue, result, "Clamping result \(result) should not equal initial in-range value \(initialValue).")
+        let expected = ClosedRangeClampTests.characterRange.upperBound
+        XCTAssertEqual(result, expected, "Clamping result \(result) should equal expected value \(expected).")
+    }
+    
+    func testCharacterClosedRangeClampBelowRange()
+    {
+        let initialValue : Character = "a"
+        let result = ClosedRangeClampTests.characterRange.clamp(initialValue)
+        XCTAssertNotEqual(initialValue, result, "Clamping result \(result) should not equal initial in-range value \(initialValue).")
+        let expected = ClosedRangeClampTests.characterRange.lowerBound
+        XCTAssertEqual(result, expected, "Clamping result \(result) should equal expected value \(expected).")
+    }
+    
+    func testCharacterClosedRangeClampOnUpperBound()
+    {
+        let initialValue = ClosedRangeClampTests.characterRange.upperBound
+        let result = ClosedRangeClampTests.characterRange.clamp(initialValue)
+        XCTAssertEqual(initialValue, result, "Clamping result \(result) should equal initial in-range value \(initialValue)")
+    }
+    
+    func testCharacterClosedRangeClampOnLowerBound()
+    {
+        let initialValue = ClosedRangeClampTests.characterRange.lowerBound
+        let result = ClosedRangeClampTests.characterRange.clamp(initialValue)
+        XCTAssertEqual(initialValue, result, "Clamping result \(result) should equal initial in-range value \(initialValue)")
+    }
 }
